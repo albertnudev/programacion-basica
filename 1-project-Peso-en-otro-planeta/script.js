1af8000ae4d26eaa874d93c29e629822a64c33f0
albernucom <nunezalberto.210@gmail.com>
2021-06-02 13:33:42 -0500

    Project 1 done

--- 1-project-Peso-en-otro-planeta/script.js
+++ 1-project-Peso-en-otro-planeta/script.js
@@ -6,14 +6,19 @@ let g_tierra = 9.8;
 let g_marte = 3.7;
 let g_jupiter = 24.8;
 let peso_final;
+let nombre = ''
 if(planeta == 1){
     peso_final = peso * g_marte / g_tierra;
+    nombre = 'Marte'
 }
 else if(planeta == 2){
     peso_final = peso * g_jupiter / g_tierra;
+    nombre = 'Jupiter'
 } else {
     peso_final = 1000000;
+    nombre = 'Krypton'
+
 }
-peso_final = peso * g_marte / g_tierra;
 peso_final = parseInt(peso_final);
-document.write('Tu peso en marte es <strong>' + peso_final + ' kilos</strong>');
+
+document.write('Tu peso en ' + nombre + ' es <strong>' + peso_final + ' kilos</strong>');
